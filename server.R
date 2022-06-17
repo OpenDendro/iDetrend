6c3e789eb046cb48e3675af771456e78dd848292
Andy Bunn <andy.bunn@wwu.edu>
2022-06-17 16:04:31 -0700

    still setting up project

--- server.R
+++ server.R
@@ -9,39 +9,39 @@ library(shinyjs)
 library(gridExtra)
 
 shinyServer(function(session, input, output) {
-  
+
   # Declare a few things
-  
+
   # Initiate an object (empty now) that will hold the rwl data
-  # (and a backup). The advantage of this is that it can be edited 
+  # (and a backup). The advantage of this is that it can be edited
   # and saved between tabs
   rwlRV <- reactiveValues()
   # Here are the elements of rwlRV. These don't need to be declared here
   # but I want to keep track of what's in the object as a matter of best
   # practices
-  
+
   rwlRV$theRWL <- NULL           # the rwl object
   rwlRV$theSeriesDF <- NULL      # the series as a df
   rwlRV$Curve <- NULL            # the curve
   rwlRV$Fits <- NULL             # the fits
   rwlRV$ModelInfo <- NULL        # modelInfo
-  
+
   ##############################################################
   #
   # START observations
   #
   ##############################################################
-  
-  
+
+
   # When app is initiated, hide all the tabs but the first one.
   # This creates an observer so that they can be toggled when triggered
   # by an event
-  
+
   # get series names
   observeEvent(
     eventExpr = {
       getRWL()
-    }, 
+    },
     handlerExpr = {
       updateSelectInput(session = session,
                         inputId = "series",
@@ -49,16 +49,16 @@ shinyServer(function(session, input, output) {
                         selected=colnames(rwlRV$theRWL[1]))
     },
     label = "observe series being selected and update the dropdown")
-  
-  
-  
+
+
+
   ##############################################################
   #
   # END observations
   #
   ##############################################################
-  
-  
+
+
   ##############################################################
   #
   # START reactives
@@ -67,7 +67,7 @@ shinyServer(function(session, input, output) {
   # need to be done only once.
   #
   ##############################################################
-  
+
   # Get the RWL file from the user at the start or use demo data
   getRWL <- reactive({
     if (input$useDemoDated) {
@@ -87,9 +87,9 @@ shinyServer(function(session, input, output) {
       rwlRV$theRWL <- dat
       return(dat)
     }
-    
+
   })
-  
+
   # Get a series to use
   getSeries <- reactive({
     req(getRWL())
@@ -100,8 +100,8 @@ shinyServer(function(session, input, output) {
       mutate(index = 1:length(yrs))
     rwlRV$theSeriesDF <- df
   })
-  
-  # do the detrending 
+
+  # do the detrending
   detrendSelectedSeries <- reactive({
     req(getSeries())
     df <- rwlRV$theSeriesDF
@@ -114,68 +114,68 @@ shinyServer(function(session, input, output) {
     f2use = 0.5
     pos.slope2use = FALSE
     constrain.nls2use = c("never", "when.fail", "always")
-    difference2use <- input$difference
-    
+    difference2use <- ifelse(input$differenceText == "Difference",TRUE,FALSE)
+
     # update args for each method
     if(method2use == "AgeDepSpline"){
-      nyrs2use <- input$nyrADS  
+      nyrs2use <- input$nyrADS
       pos.slope2use <- input$pos.slopeADS
     }
-    
+
     if(method2use == "Spline"){
-      nyrs2use <- input$nyrsCAPS  
-      f2use <- input$f 
+      nyrs2use <- input$nyrsCAPS
+      f2use <- input$f
     }
-    
+
     if(method2use == "ModNegExp"){
       pos.slope2use <- input$pos.slopeModNegExp
       constrain.nls2use = input$constrain.nls2useModNegExp
     }
-    #wt, span = "cv", bass = 0, 
-    
-    res <- detrend.series(y = df$aSeries, 
+    #wt, span = "cv", bass = 0,
+
+    res <- detrend.series(y = df$aSeries,
                                        method = method2use,
                                        nyrs = nyrs2use,
                                        f = f2use,
                                        pos.slope = pos.slope2use,
                                        constrain.nls = constrain.nls2use,
                                        make.plot = FALSE,
-                                       verbose = FALSE, 
+                                       verbose = FALSE,
                                        return.info = TRUE,
-                                       difference = difference2use) 
+                                       difference = difference2use)
     rwlRV$Curve <- res$curve
     rwlRV$Fits <- res$series
     rwlRV$ModelInfo <- res$model.info
     #return(res)
   })
-  
-  
-  
+
+
+
   ##############################################################
   #
   # END reactives
   #
   ##############################################################
-  
-  
+
+
   ##############################################################
   #
   # 2nd tab Describe RWL Data
   #
   ##############################################################
-  
+
   # -- get the RWL report
   output$rwlReport <- renderPrint({
     req(getRWL())
     rwl.report(rwlRV$theRWL)
   })
-  
+
   # -- plot rwl
   output$rwlPlot <- renderPlot({
     req(getRWL())
     plot.rwl(rwlRV$theRWL,plot.type = input$rwlPlotType)
   })
-  
+
   # -- summary
   output$rwlSummary <- renderTable({
     req(getRWL())
@@ -185,14 +185,14 @@ shinyServer(function(session, input, output) {
   output$rwlSummaryReport <- downloadHandler(
     filename = "rwl_summary_report.html",
     content = function(file) {
-      
+
       tempReport <- file.path(tempdir(), "report_rwl_describe.Rmd")
       file.copy("report_rwl_describe.Rmd", tempReport, overwrite = TRUE)
-      
+
       rwlObject <- rwlRV$theRWL
       params <- list(fileName = input$file1$name, rwlObject=rwlObject,
                      rwlPlotType=input$rwlPlotType)
-      
+
       # Knit the document, passing in the `params` list, and eval it in a
       # child of the global environment (this isolates the code in the document
       # from the code in this app). Defensive
@@ -202,7 +202,7 @@ shinyServer(function(session, input, output) {
       )
     }
   )
-  
+
   ##############################################################
   #
   # 3rd tab Detrend those things
@@ -222,29 +222,29 @@ shinyServer(function(session, input, output) {
       geom_line(aes(x=index,y=aSeries)) +
       geom_line(aes(x=index,y=Curve)) +
       scale_x_continuous(name = "Index",position = "top")
-    
+
     pFits <- ggplot(seriesDF) +
       geom_line(aes(x=index,y=Fits)) +
       scale_x_continuous(name = "Index")
-    
-    # make sure the axes are the same precision. 
-    pSeries <- pSeries + 
+
+    # make sure the axes are the same precision.
+    pSeries <- pSeries +
       scale_y_continuous(labels = scales::number_format(accuracy = 0.01))
-    
-    pFits <- pFits + 
+
+    pFits <- pFits +
       scale_y_continuous(labels = scales::number_format(accuracy = 0.01))
-    
-    
+
+
     grid.arrange(pSeries,pFits)
-    
+
   })
-  
-  output$detrendInfo <- renderPrint({ 
+
+  output$detrendInfo <- renderPrint({
     req(getSeries())
     req(detrendSelectedSeries())
-    
+
     rwlRV$ModelInfo
     })
-  
+
 })
 
--- ui.R
+++ ui.R
@@ -26,7 +26,7 @@ ui <- tagList(
                  includeMarkdown("text_upload.rmd"),
                  hr(),
                  h5("RWL File"),
-                 fileInput(inputId="file1", 
+                 fileInput(inputId="file1",
                            label=NULL,
                            multiple = FALSE,
                            accept = c("text/plain",
@@ -36,7 +36,7 @@ ui <- tagList(
                  checkboxInput(inputId="useDemoDated", label="Or use example data",
                                value=TRUE),
                ),
-               
+
                # Main panel for displaying outputs
                mainPanel(
                  includeMarkdown("text_intro.rmd")
@@ -64,81 +64,95 @@ ui <- tagList(
     # 3rd tab Detrend  ----
     tabPanel(title="3. Detrend",value="DetrendTab",
              # choose series
-             selectInput(inputId = "series",
-                         label = "Choose series",
-                         choices = c("foo")),
-             
+             fluidRow(
+               column(4,
+                      selectInput(inputId = "series",
+                                  label = "Choose series",
+                                  choices = c("foo"))
+               ),
+               column(4,
+                      selectInput(inputId = "differenceText",
+                             label = "Residual Method",
+                             choices = c("Division","Difference"),
+                             selected = "Ratio")
+               )
+             ), # end row
              # plot series
              plotOutput(outputId = "seriesPlot"),
              # info on method
-             verbatimTextOutput(outputId = "detrendInfo"),
-             
-             # choose method
-             selectInput(inputId = "detrendMethod",label = "Choose method",
-                         choices = c("AgeDepSpline", "Spline", "ModNegExp", "Mean",
-                                     "Ar", "Friedman", "ModHugershoff"),
-                         selected = "AgeDepSpline"),
-             
-             checkboxInput(inputId = "difference", 
-                           label = "Compute with difference",
-                           value = FALSE),
-             
-             # conditional arguments
-             conditionalPanel(condition = "input.detrendMethod == 'Spline'",
-                              numericInput(inputId = "nyrsCAPS", 
-                                           label = "Spline Stiffness",
-                                           value = 20, 
-                                           min = 10,
-                                           max=1e3,
-                                           step = 10),
-                              numericInput(inputId = "f", 
-                                           label = "Frequency response",
-                                           value = 0.5,
-                                           min = 0,
-                                           max=1,
-                                           step = 0.05)),
-             
-             conditionalPanel(condition = "input.detrendMethod == 'AgeDepSpline'",
-                              numericInput(inputId = "nyrsADS", 
-                                           label = "Initial Spline Stiffness",
-                                           value = 50, 
-                                           min = 1,
-                                           max=200,
-                                           step = 1),
-                              checkboxInput(inputId = "pos.slopeADS", 
-                                            label = "Allow Positive Slope",
-                                            value = FALSE)),
-             
-             
-             conditionalPanel(condition = "input.detrendMethod == 'ModNegExp'",
-                              selectInput(inputId = "constrain.nlsModNegExp", 
-                                          label = "When should the parameters of 
+             fluidRow(
+               column(1),
+               column(4,
+
+                      # choose method
+                      selectInput(inputId = "detrendMethod",label = "Choose method",
+                                  choices = c("AgeDepSpline", "Spline", "ModNegExp", "Mean",
+                                              "Ar", "Friedman", "ModHugershoff"),
+                                  selected = "AgeDepSpline"),
+
+
+                      # conditional arguments
+                      conditionalPanel(condition = "input.detrendMethod == 'Spline'",
+                                       numericInput(inputId = "nyrsCAPS",
+                                                    label = "Spline Stiffness",
+                                                    value = 20,
+                                                    min = 10,
+                                                    max=1e3,
+                                                    step = 10),
+                                       numericInput(inputId = "f",
+                                                    label = "Frequency response",
+                                                    value = 0.5,
+                                                    min = 0,
+                                                    max=1,
+                                                    step = 0.05)),
+
+                      conditionalPanel(condition = "input.detrendMethod == 'AgeDepSpline'",
+                                       numericInput(inputId = "nyrsADS",
+                                                    label = "Initial Spline Stiffness",
+                                                    value = 50,
+                                                    min = 1,
+                                                    max=200,
+                                                    step = 1),
+                                       checkboxInput(inputId = "pos.slopeADS",
+                                                     label = "Allow Positive Slope",
+                                                     value = FALSE)),
+
+
+                      conditionalPanel(condition = "input.detrendMethod == 'ModNegExp'",
+                                       selectInput(inputId = "constrain.nlsModNegExp",
+                                                   label = "When should the parameters of
                                           the nls function be constrained?",
                                           choices = c("never","when.fail",
                                                       "always"),
                                           selected = "never"),
-                              checkboxInput(inputId = "pos.slopeModNegExp", 
-                                            label = "Allow Positive Slope",
-                                            value = FALSE)),
-             
-             conditionalPanel(condition = "input.detrendMethod == 'ModHugershoff'",
-                              selectInput(inputId = "constrain.nlsModHugershoff", 
-                                          label = "When should the parameters of 
+                                       checkboxInput(inputId = "pos.slopeModNegExp",
+                                                     label = "Allow Positive Slope",
+                                                     value = FALSE)),
+
+                      conditionalPanel(condition = "input.detrendMethod == 'ModHugershoff'",
+                                       selectInput(inputId = "constrain.nlsModHugershoff",
+                                                   label = "When should the parameters of
                                           the nls function be constrained?",
                                           choices = c("never","when.fail",
                                                       "always"),
                                           selected = "never")),
-             
-             conditionalPanel(condition = "input.detrendMethod == 'Friedman'",
-                              numericInput(inputId = "wt", 
-                                           label = "case weights, by default all equal",
-                                           value = NULL, min = 0,max=1,step = 0.05),
-                              numericInput(inputId = "span", 
-                                           label = "the fraction of the observations in the span",
-                                           value = NULL,min = 0,max=1,step = 0.05),
-                              numericInput(inputId = "bass", 
-                                           label = "smoothness of the fitted curve",
-                                           value = 0,min = 0,max=10,step = 1))
-    )
+
+                      conditionalPanel(condition = "input.detrendMethod == 'Friedman'",
+                                       numericInput(inputId = "wt",
+                                                    label = "case weights, by default all equal",
+                                                    value = NULL, min = 0,max=1,step = 0.05),
+                                       numericInput(inputId = "span",
+                                                    label = "the fraction of the observations in the span",
+                                                    value = NULL,min = 0,max=1,step = 0.05),
+                                       numericInput(inputId = "bass",
+                                                    label = "smoothness of the fitted curve",
+                                                    value = 0,min = 0,max=10,step = 1))
+               ),
+               column(6,
+                      verbatimTextOutput(outputId = "detrendInfo")
+               ),
+               column(1)
+             )
+    ) # end row
   )# end tabs
 )
